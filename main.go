f32d573ec3dae75fbb4443c7a71e2e5f4b8253d8
yota-hda <hadayota33@gmail.com>
2019-04-04 12:23:11 +0900

    feat: solve test of ATCoder.

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var value string
 	fmt.Scan(&value)
 
-	basePattern := map[string]string{
-		"A": "T",
-		"T": "A",
-		"G": "C",
-		"C": "G",
+	var count, maxLength int
+	var isMatched bool
+
+	targetChars := [4]string{"A", "T", "C", "G"}
+
+	for _, char := range value {
+		isMatched = false
+		for _, targetChar := range targetChars {
+			if targetChar == string(char) {
+				isMatched = true
+			}
+		}
+
+		if isMatched {
+			count++
+		} else {
+			count = 0
+		}
+
+		if count > maxLength {
+			maxLength = count
+		}
 	}
 
-	fmt.Println(basePattern[value])
+	fmt.Println(maxLength)
 }
