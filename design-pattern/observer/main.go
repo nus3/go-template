80353ce188f76d0e2b84052d99f00e3d8296d0d8
yota-hada <hadayota33@gmail.com>
2019-07-02 19:16:57 +0900

    temp

---
+++ design-pattern/observer/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+// Observer is
+type Observer interface {
+	Update()
+}
+
+// Generator is
+type Generator interface {
+	AddObserver(Observer)
+	// TODO: 削除
+	// DeleteObserver(Observer)
+	NotifyObservers(Observer)
+	GetNumber() int
+	Execute()
+}
+
+// NumberGenerator is
+type NumberGenerator struct {
+	observers []*Observer
+	number    int
+}
+
+// AddObserver returns
+func (n *NumberGenerator) AddObserver(o *Observer) {
+	n.observers = append(n.observers, o)
+}
+
+// NotifyObservers returns
+func (n *NumberGenerator) NotifyObservers() {
+	for _, observer := range n.observers {
+		observer.Update()
+	}
+}
+
+// GetNumber returns
+func (n *NumberGenerator) GetNumber() int {
+	return n.number
+}
+
+// Execute returns
+func (n *NumberGenerator) Execute() {
+	for index := 0; index < 20; index++ {
+		n.number = rand.Intn(15)
+		n.NotifyObservers()
+	}
+}
+
+// DegitObserver is
+type DegitObserver struct{}
+
+// Update returns
+func (d DegitObserver) Update(g *Generator) {
+	fmt.Println(g.GetNumber())
+}
+
+// GraphObserver is
+type GraphObserver struct{}
+
+// Update returns
+func (gr GraphObserver) Update(n *Generator) {
+	count := n.GetNumber()
+	output := ""
+
+	for index := 0; index < count; index++ {
+		output = output + "*"
+	}
+
+	fmt.Println(output)
+}
+
+func main() {
+	generator := &NumberGenerator{}
+	var d Observer = DegitObserver{}
+	var g Observer = GraphObserver{}
+
+	generator.AddObserver(d)
+	generator.AddObserver(g)
+	generator.Execute()
+}
---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/yota-hada/go-template
+
+go 1.12
+
+require (
+	github.com/gin-contrib/sse v0.0.0-20190301062529-5545eab6dad3
+	github.com/gin-gonic/gin v1.3.0
+	github.com/golang/protobuf v1.3.1
+	github.com/json-iterator/go v1.1.6
+	github.com/mattn/go-isatty v0.0.7
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742
+	github.com/ugorji/go v1.1.2
+	golang.org/x/sys v0.0.0-20190402142545-baf5eb976a8c
+	gopkg.in/go-playground/validator.v8 v8.18.2
+	gopkg.in/yaml.v2 v2.2.2
+)
