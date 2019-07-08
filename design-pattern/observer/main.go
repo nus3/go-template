e113ce976de19434f59c17e13ae9ad06c9a86b5f
yota-hada <hadayota33@gmail.com>
2019-07-09 00:28:21 +0900

    feat: add observer design pattern.

--- design-pattern/observer/main.go
+++ design-pattern/observer/main.go
@@ -3,53 +3,68 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 // Observer is
 type Observer interface {
-	Update()
+	Update(Subject)
 }
 
-// Generator is
-type Generator interface {
+// Subject is
+type Subject interface {
 	AddObserver(Observer)
-	// TODO: 削除
-	// DeleteObserver(Observer)
-	NotifyObservers(Observer)
+	DeleteObserver(Observer)
+	NotifyObservers()
 	GetNumber() int
 	Execute()
 }
 
 // NumberGenerator is
 type NumberGenerator struct {
-	observers []*Observer
+	observers []Observer
 	number    int
 }
 
 // AddObserver returns
-func (n *NumberGenerator) AddObserver(o *Observer) {
-	n.observers = append(n.observers, o)
+func (ng *NumberGenerator) AddObserver(o Observer) {
+	ng.observers = append(ng.observers, o)
 }
 
-// TODO: observer
+// DeleteObserver returns
+func (ng *NumberGenerator) DeleteObserver(o Observer) {
+	result := NumberGenerator{}
+	for _, observer := range ng.observers {
+		if observer != o {
+			result.observers = append(result.observers, o)
+		}
+	}
+	ng.observers = result.observers
+}
 
 // NotifyObservers returns
-func (n *NumberGenerator) NotifyObservers() {
-	for _, observer := range n.observers {
-		observer.Update()
+func (ng *NumberGenerator) NotifyObservers() {
+	for _, observer := range ng.observers {
+		observer.Update(ng)
 	}
 }
 
 // GetNumber returns
-func (n *NumberGenerator) GetNumber() int {
-	return n.number
+func (ng *NumberGenerator) GetNumber() int {
+	return ng.number
 }
 
 // Execute returns
-func (n *NumberGenerator) Execute() {
-	for index := 0; index < 20; index++ {
-		n.number = rand.Intn(15)
-		n.NotifyObservers()
+func (ng *NumberGenerator) Execute() {
+	rand.Seed(time.Now().UnixNano())
+
+	// NOTE: randomな時間、numberを更新する
+	for index := 0; index < 5; index++ {
+		ng.number = rand.Intn(50)
+		ng.NotifyObservers()
+
+		st := rand.Intn(10)
+		time.Sleep(time.Duration(st) * time.Second)
 	}
 }
 
@@ -57,31 +72,13 @@ func (n *NumberGenerator) Execute() {
 type DegitObserver struct{}
 
 // Update returns
-func (d DegitObserver) Update(n *NumberGenerator) {
-	fmt.Println(n.GetNumber())
-}
-
-// GraphObserver is
-type GraphObserver struct{}
-
-// Update returns
-func (gr GraphObserver) Update(n *NumberGenerator) {
-	count := n.GetNumber()
-	output := ""
-
-	for index := 0; index < count; index++ {
-		output = output + "*"
-	}
-
-	fmt.Println(output)
+func (d *DegitObserver) Update(s Subject) {
+	fmt.Println("DegitObserverが状態が変化したことを検知したよ: ", s.GetNumber())
 }
 
 func main() {
-	generator := &NumberGenerator{}
-	var d Observer = &DegitObserver{}
-	var g Observer = &GraphObserver{}
-
-	generator.AddObserver(d)
-	generator.AddObserver(g)
-	generator.Execute()
+	ng := &NumberGenerator{}
+	do := &DegitObserver{}
+	ng.AddObserver(do)
+	ng.Execute()
 }
